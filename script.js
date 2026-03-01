9d498a923e03e8ede0a19539a7acb6629206c876
Jackson202-svg <zwtwwkg65k@privaterelay.appleid.com>
2026-03-02 07:01:01 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("US & Iran News 2026 website loaded.");
+
+document.addEventListener("DOMContentLoaded", function() {
+    alert("Welcome to US & Iran News 2026!");
+});console.log("US & Iran News 2026 website loaded.");
+
+document.addEventListener("DOMContentLoaded", function() {
+    alert("Welcome to US & Iran News 2026!");
+});
